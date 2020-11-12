9b83de6159d5623753abb1b546731f901e829fba
Inusha Manawadu <66345099+InushaManawadu@users.noreply.github.com>
2020-11-13 02:39:06 +0530

    Footer
    Footer added

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from "react";
 import Banner from "./components/Banner";
 import "./styles/reset.css";
 import "./styles/banner.css";
+import "./styles/footer.css";
 import Talents from "./components/Talents";
 import NavigationBar from "./components/NavigationBar";
+import Footer from "./components/Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
@@ -12,6 +14,7 @@ function App() {
       <NavigationBar />
       <Banner />
       <Talents />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import "../styles/footer.css";
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <div className="footer">
+                <div className="footer__info">
+                    <p>Copyright &copy; Inusha Manawadu</p>
+                </div>
+            </div>
+        )
+    }
+}
