3774fa9960abecadb46eac9e1401d84e97594d18
Inusha Manawadu <66345099+InushaManawadu@users.noreply.github.com>
2020-11-13 01:55:47 +0530

    Updates

---
+++ precache-manifest.76d48d6e22e8bea185eac891af15f6ea.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b83f55a90191ef28e3d3c6e5a62161a6",
+    "url": "/Inusha-portfolio/index.html"
+  },
+  {
+    "revision": "265c1d8679b03df28270",
+    "url": "/Inusha-portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "4e30fa503221e1bf9c11",
+    "url": "/Inusha-portfolio/static/css/main.a48009d0.chunk.css"
+  },
+  {
+    "revision": "265c1d8679b03df28270",
+    "url": "/Inusha-portfolio/static/js/2.ab367ffd.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/Inusha-portfolio/static/js/2.ab367ffd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e30fa503221e1bf9c11",
+    "url": "/Inusha-portfolio/static/js/main.9c3b6022.chunk.js"
+  },
+  {
+    "revision": "b7bd0bd6b4178c91cf8b",
+    "url": "/Inusha-portfolio/static/js/runtime-main.eda48b4c.js"
+  },
+  {
+    "revision": "e693fd6b7438de2388ab01478f6bde03",
+    "url": "/Inusha-portfolio/static/media/background-1.e693fd6b.png"
+  },
+  {
+    "revision": "10f347689aa8005a7bbaf19fec0b875e",
+    "url": "/Inusha-portfolio/static/media/frontend.10f34768.png"
+  }
+]);
